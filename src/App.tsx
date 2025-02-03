d289fec1308dc72083ed96442027484ef440fb42
yash <yashraj.ch2048@gmail.com>
2025-02-03 16:50:01 +0530

    Moved App.tsx, added a Footer Component amd deleted Gemfile since I'm not developing for ios

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import Footer from './components/Footer'
+
+export default function App(){
+  return (
+    <View>
+      
+      <Footer/>
+    </View>
+  )
+}
