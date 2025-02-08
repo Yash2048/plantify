2d5725fa1f631371f64759e84a89eca685e052a8
yash <yashraj.ch2048@gmail.com>
2025-02-09 04:03:05 +0530

    Update Footer component to include a TouchableOpacity button and adjust styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <View className=' flex items-center justify-center h-16 bg-gray-800'>
-      <Text className='text-white'>Footer</Text>
+    <View className='flex items-center justify-center h-20 bg-gray-800  '>
+      <TouchableOpacity className='bg-blue-400 rounded-full w-[60px] h-[60px] border-red-700 border-[6px]' />
     </View>
   )
+    
 }
